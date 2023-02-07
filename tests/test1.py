a682d5201ff2068305df38a3f165d1331fda97f4
rharkes <rolf@harkes.nu>
2023-02-07 02:23:26 +0100

    black on test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -6,16 +6,20 @@ from flifile.readheader import readheader
 
 @pytest.fixture
 def file():
-    return Path(Path.cwd(), 'testdata', 'header.txt')
+    return Path(Path.cwd(), "testdata", "header.txt")
 
 
 @pytest.fixture
 def header():
-    return {'CAMERADATA': {'something': {'why': 'because'}},
-            'FLIMDATA': {'background': {'x': '123', 'y': '32'},
-                         'image': {'x': '1', 'y': '2'}},
-            'datastart': 93}
+    return {
+        "CAMERADATA": {"something": {"why": "because"}},
+        "FLIMDATA": {
+            "background": {"x": "123", "y": "32"},
+            "image": {"x": "1", "y": "2"},
+        },
+        "datastart": 93,
+    }
 
 
 def testheader(file, header):
-    assert (readheader(file) == header)
+    assert readheader(file) == header
