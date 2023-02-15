fcd21bb3a89de7dffb4812712cf80284d64f3ce7
Ids Schiere <Ids@lambert.local>
2023-02-15 10:44:33 +0100

    formatting

--- flifile/flifile.py
+++ flifile/flifile.py
@@ -372,7 +372,7 @@ class FliFile:
             self.header["FLIMIMAGE"]["DEFAULT"].get("numberOfDarkImages", 0)
         )
         data_info['BG_present'] = nrOfDarkImages > 0
-        data_info['BGSize'] = list(data_info['IMSize'])
+    data_info['BGSize'] = list(data_info['IMSize'])
         data_info['BGType'] = data_info['IMType']
         data_info['BGPacking'] = data_info['IMPacking']
         data_info['Compression'] = 0
